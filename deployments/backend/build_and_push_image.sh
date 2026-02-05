064e208dc7802eea62b55666c1cee33d24cfcf31
prerana <prerana.chaks@gmail.com>
2026-02-05 23:11:54 +0800

    initial dockerfile

---
+++ deployments/backend/build_and_push_image.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+# -------- CONFIG --------
+DOCKERHUB_USER="prerana1205"
+IMAGE_NAME="recolor-api"
+VERSION="v1"
+
+# -------- DERIVED --------
+IMAGE_URI="${DOCKERHUB_USER}/${IMAGE_NAME}:${VERSION}"
+
+# -------- COPY --------
+cp -r ../inference ./inference
+BUILD_DIR="."
+
+# -------- BUILD --------
+echo "🐳 Building image: ${IMAGE_URI}"
+docker build -t "${IMAGE_URI}" "${BUILD_DIR}"
+
+# -------- PUSH --------
+echo "🚀 Pushing image to Docker Hub"
+docker push "${IMAGE_URI}"
+
+# -------- CLEANUP --------
+rm -rf ./inference
+
+echo "✅ Done!"
+echo "Image pushed: ${IMAGE_URI}"
