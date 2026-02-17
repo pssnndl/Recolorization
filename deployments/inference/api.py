9b6b084f8de0aec18352090823bc8bf2a0aa4468
sneha <30837089+itmesneha@users.noreply.github.com>
2026-02-18 00:50:00 +0800

    added vercel url for cors backend

--- deployments/inference/api.py
+++ deployments/inference/api.py
@@ -19,7 +19,11 @@ app = FastAPI()
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # In production, replace with specific origins!!
+    allow_origins=[
+        "https://recolorization.vercel.app",
+        # "http://localhost:5173",
+        # "http://localhost:3000"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
